bffbeef5ac87ae2e2fa3dad4985c329422474095
Andrey Tkachenko <falko.lab@gmail.com>
2015-11-15 23:41:03 +0300

    ti sdk fixes

--- xml2json.js
+++ xml2json.js
@@ -1,4 +1,8 @@
 /*
+ This is adapter version for Titanium SDK by Andrey Tkacheko aka falkolab
+*/
+
+/*
  Copyright 2011-2013 Abdulla Abdurakhmanov
  Original sources are available at https://code.google.com/p/x2js/
 
@@ -15,15 +19,15 @@
  limitations under the License.
  */
 
-function X2JS(config) {
+exports.X2JS = function X2JS(config) {
 	'use strict';
-		
-	var VERSION = "1.1.7";
-	
+
+	//var VERSION = "1.1.7";
+
 	config = config || {};
 	initConfigDefaults();
 	initRequiredPolyfills();
-	
+
 	function initConfigDefaults() {
 		if(config.escapeMode === undefined) {
 			config.escapeMode = true;
@@ -32,9 +36,9 @@ function X2JS(config) {
 		config.arrayAccessForm = config.arrayAccessForm || "none";
 		config.emptyNodeForm = config.emptyNodeForm || "text";
 		if(config.enableToStringFunc === undefined) {
-			config.enableToStringFunc = true; 
+			config.enableToStringFunc = true;
 		}
-		config.arrayAccessFormPaths = config.arrayAccessFormPaths || []; 
+		config.arrayAccessFormPaths = config.arrayAccessFormPaths || [];
 		if(config.skipEmptyTextNodesForObj === undefined) {
 			config.skipEmptyTextNodesForObj = true;
 		}
@@ -48,14 +52,6 @@ function X2JS(config) {
 		}
 	}
 
-	var DOMNodeTypes = {
-		ELEMENT_NODE 	   : 1,
-		TEXT_NODE    	   : 3,
-		CDATA_SECTION_NODE : 4,
-		COMMENT_NODE	   : 8,
-		DOCUMENT_NODE 	   : 9
-	};
-	
 	function initRequiredPolyfills() {
 		function pad(number) {
 	      var r = String(number);
@@ -64,40 +60,27 @@ function X2JS(config) {
 	      }
 	      return r;
 	    }
-		// Hello IE8-
-		if(typeof String.prototype.trim !== 'function') {			
+
+		if(typeof String.prototype.trim !== 'function') {
 			String.prototype.trim = function() {
 				return this.replace(/^\s+|^\n+|(\s|\n)+$/g, '');
-			}
+			};
 		}
 		if(typeof Date.prototype.toISOString !== 'function') {
 			// Implementation from http://stackoverflow.com/questions/2573521/how-do-i-output-an-iso-8601-formatted-string-in-javascript
 			Date.prototype.toISOString = function() {
-		      return this.getUTCFullYear()
-		        + '-' + pad( this.getUTCMonth() + 1 )
-		        + '-' + pad( this.getUTCDate() )
-		        + 'T' + pad( this.getUTCHours() )
-		        + ':' + pad( this.getUTCMinutes() )
-		        + ':' + pad( this.getUTCSeconds() )
-		        + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
-		        + 'Z';
+		      return this.getUTCFullYear() +
+		        '-' + pad( this.getUTCMonth() + 1 ) +
+		        '-' + pad( this.getUTCDate() ) +
+		        'T' + pad( this.getUTCHours() ) +
+		        ':' + pad( this.getUTCMinutes() ) +
+		        ':' + pad( this.getUTCSeconds() ) +
+		        '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 ) +
+		        'Z';
 		    };
 		}
 	}
-	
-	function getNodeLocalName( node ) {
-		var nodeLocalName = node.localName;			
-		if(nodeLocalName == null) // Yeah, this is IE!! 
-			nodeLocalName = node.baseName;
-		if(nodeLocalName == null || nodeLocalName=="") // =="" is IE too
-			nodeLocalName = node.nodeName;
-		return nodeLocalName;
-	}
-	
-	function getNodePrefix(node) {
-		return node.prefix;
-	}
-		
+
 	function escapeXmlChars(str) {
 		if(typeof(str) == "string")
 			return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;');
@@ -108,7 +91,7 @@ function X2JS(config) {
 	function unescapeXmlChars(str) {
 		return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&#x27;/g, "'").replace(/&amp;/g, '&');
 	}
-	
+
 	function toArrayAccessForm(obj, childName, path) {
 		switch(config.arrayAccessForm) {
 		case "property":
@@ -116,11 +99,11 @@ function X2JS(config) {
 				obj[childName+"_asArray"] = [obj[childName]];
 			else
 				obj[childName+"_asArray"] = obj[childName];
-			break;		
+			break;
 		/*case "none":
 			break;*/
 		}
-		
+
 		if(!(obj[childName] instanceof Array) && config.arrayAccessFormPaths.length > 0) {
 			var idx = 0;
 			for(; idx < config.arrayAccessFormPaths.length; idx++) {
@@ -133,7 +116,7 @@ function X2JS(config) {
 				if( arrayPath instanceof RegExp) {
 					if(arrayPath.test(path))
 						break;
-				}				
+				}
 				else
 				if( typeof arrayPath === "function") {
 					if(arrayPath(obj, childName, path))
@@ -145,13 +128,13 @@ function X2JS(config) {
 			}
 		}
 	}
-	
+
 	function fromXmlDateTime(prop) {
 		// Implementation based up on http://stackoverflow.com/questions/8178598/xml-datetime-to-javascript-date-object
 		// Improved to support full spec and optional parts
 		var bits = prop.split(/[-T:+Z]/g);
-		
-		var d = new Date(bits[0], bits[1]-1, bits[2]);			
+
+		var d = new Date(bits[0], bits[1]-1, bits[2]);
 		var secondBits = bits[5].split("\.");
 		d.setHours(bits[3], bits[4], secondBits[0]);
 		if(secondBits.length>1)
@@ -166,17 +149,17 @@ function X2JS(config) {
 			offsetMinutes = 0 + (sign == '-'? -1 * offsetMinutes : offsetMinutes);
 
 			// Apply offset and local timezone
-			d.setMinutes(d.getMinutes() - offsetMinutes - d.getTimezoneOffset())
+			d.setMinutes(d.getMinutes() - offsetMinutes - d.getTimezoneOffset());
 		}
 		else
 			if(prop.indexOf("Z", prop.length - 1) !== -1) {
-				d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()));					
+				d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()));
 			}
 
 		// d is now a local time equivalent to the supplied time
 		return d;
 	}
-	
+
 	function checkFromXmlDateTimePaths(value, childName, fullPath) {
 		if(config.datetimeAccessFormPaths.length > 0) {
 			var path = fullPath.split("\.#")[0];
@@ -191,7 +174,7 @@ function X2JS(config) {
 				if( dtPath instanceof RegExp) {
 					if(dtPath.test(path))
 						break;
-				}				
+				}
 				else
 				if( typeof dtPath === "function") {
 					if(dtPath(obj, childName, path))
@@ -209,36 +192,37 @@ function X2JS(config) {
 	}
 
 	function parseDOMChildren( node, path ) {
-		if(node.nodeType == DOMNodeTypes.DOCUMENT_NODE) {
-			var result = new Object;
-			var nodeChildren = node.childNodes;
+		var result, child, cidx, nodeChildren, childName;
+		if(node.nodeType == Titanium.XML.Node.DOCUMENT_NODE) {
+			result = {};
+			nodeChildren = node.childNodes;
 			// Alternative for firstElementChild which is not supported in some environments
-			for(var cidx=0; cidx <nodeChildren.length; cidx++) {
-				var child = nodeChildren.item(cidx);
-				if(child.nodeType == DOMNodeTypes.ELEMENT_NODE) {
-					var childName = getNodeLocalName(child);
+			for(cidx=0; cidx <nodeChildren.length; cidx++) {
+				child = nodeChildren.item(cidx);
+				if(child.nodeType == Titanium.XML.Node.ELEMENT_NODE) {
+					childName = child.localName;
 					result[childName] = parseDOMChildren(child, childName);
 				}
 			}
 			return result;
 		}
 		else
-		if(node.nodeType == DOMNodeTypes.ELEMENT_NODE) {
-			var result = new Object;
+		if(node.nodeType == Titanium.XML.Node.ELEMENT_NODE) {
+			result = {};
 			result.__cnt=0;
-			
-			var nodeChildren = node.childNodes;
-			
+
+			nodeChildren = node.childNodes;
+
 			// Children nodes
-			for(var cidx=0; cidx <nodeChildren.length; cidx++) {
-				var child = nodeChildren.item(cidx); // nodeChildren[cidx];
-				var childName = getNodeLocalName(child);
-				
-				if(child.nodeType!= DOMNodeTypes.COMMENT_NODE) {
+			for(cidx=0; cidx <nodeChildren.length; cidx++) {
+				child = nodeChildren.item(cidx); // nodeChildren[cidx];
+				childName = child.localName;
+
+				if(child.nodeType!= Titanium.XML.Node.COMMENT_NODE) {
 					result.__cnt++;
 					if(result[childName] == null) {
 						result[childName] = parseDOMChildren(child, path+"."+childName);
-						toArrayAccessForm(result, childName, path+"."+childName);					
+						toArrayAccessForm(result, childName, path+"."+childName);
 					}
 					else {
 						if(result[childName] != null) {
@@ -249,24 +233,24 @@ function X2JS(config) {
 						}
 						(result[childName])[result[childName].length] = parseDOMChildren(child, path+"."+childName);
 					}
-				}								
+				}
 			}
-			
+
 			// Attributes
 			for(var aidx=0; aidx <node.attributes.length; aidx++) {
 				var attr = node.attributes.item(aidx); // [aidx];
 				result.__cnt++;
 				result[config.attributePrefix+attr.name]=attr.value;
 			}
-			
+
 			// Node namespace prefix
-			var nodePrefix = getNodePrefix(node);
+			var nodePrefix = node.prefix;
 			if(nodePrefix!=null && nodePrefix!="") {
 				result.__cnt++;
 				result.__prefix=nodePrefix;
 			}
-			
-			if(result["#text"]!=null) {				
+
+			if(result["#text"]!=null) {
 				result.__text = result["#text"];
 				if(result.__text instanceof Array) {
 					result.__text = result.__text.join("\n");
@@ -286,7 +270,7 @@ function X2JS(config) {
 				if(config.arrayAccessForm=="property")
 					delete result["#cdata-section_asArray"];
 			}
-			
+
 			if( result.__cnt == 1 && result.__text!=null  ) {
 				result = result.__text;
 			}
@@ -300,22 +284,22 @@ function X2JS(config) {
 					delete result.__text;
 				}
 			}
-			delete result.__cnt;			
-			
+			delete result.__cnt;
+
 			if( config.enableToStringFunc && (result.__text!=null || result.__cdata!=null )) {
 				result.toString = function() {
 					return (this.__text!=null? this.__text:'')+( this.__cdata!=null ? this.__cdata:'');
 				};
 			}
-			
+
 			return result;
 		}
 		else
-		if(node.nodeType == DOMNodeTypes.TEXT_NODE || node.nodeType == DOMNodeTypes.CDATA_SECTION_NODE) {
+		if(node.nodeType == Titanium.XML.Node.TEXT_NODE || node.nodeType == Titanium.XML.Node.CDATA_SECTION_NODE) {
 			return node.nodeValue;
-		}	
+		}
 	}
-	
+
 	function startTag(jsonObj, element, attrList, closed) {
 		var resultStr = "<"+ ( (jsonObj!=null && jsonObj.__prefix!=null)? (jsonObj.__prefix+":"):"") + element;
 		if(attrList!=null) {
@@ -337,37 +321,37 @@ function X2JS(config) {
 			resultStr+="/>";
 		return resultStr;
 	}
-	
+
 	function endTag(jsonObj,elementName) {
 		return "</"+ (jsonObj.__prefix!=null? (jsonObj.__prefix+":"):"")+elementName+">";
 	}
-	
+
 	function endsWith(str, suffix) {
 	    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 	}
-	
+
 	function jsonXmlSpecialElem ( jsonObj, jsonObjField ) {
-		if((config.arrayAccessForm=="property" && endsWith(jsonObjField.toString(),("_asArray"))) 
-				|| jsonObjField.toString().indexOf(config.attributePrefix)==0 
-				|| jsonObjField.toString().indexOf("__")==0
-				|| (jsonObj[jsonObjField] instanceof Function) )
+		if((config.arrayAccessForm=="property" && endsWith(jsonObjField.toString(),("_asArray"))) ||
+				jsonObjField.toString().indexOf(config.attributePrefix)==0 ||
+				jsonObjField.toString().indexOf("__")==0 ||
+				(jsonObj[jsonObjField] instanceof Function))
 			return true;
 		else
 			return false;
 	}
-	
+
 	function jsonXmlElemCount ( jsonObj ) {
 		var elementsCnt = 0;
 		if(jsonObj instanceof Object ) {
 			for( var it in jsonObj  ) {
 				if(jsonXmlSpecialElem ( jsonObj, it) )
-					continue;			
+					continue;
 				elementsCnt++;
 			}
 		}
 		return elementsCnt;
 	}
-	
+
 	function parseJSONAttributes ( jsonObj ) {
 		var attrList = [];
 		if(jsonObj instanceof Object ) {
@@ -379,15 +363,15 @@ function X2JS(config) {
 		}
 		return attrList;
 	}
-	
+
 	function parseJSONTextAttrs ( jsonTxtObj ) {
 		var result ="";
-		
-		if(jsonTxtObj.__cdata!=null) {										
-			result+="<![CDATA["+jsonTxtObj.__cdata+"]]>";					
+
+		if(jsonTxtObj.__cdata!=null) {
+			result+="<![CDATA["+jsonTxtObj.__cdata+"]]>";
 		}
-		
-		if(jsonTxtObj.__text!=null) {			
+
+		if(jsonTxtObj.__text!=null) {
 			if(config.escapeMode)
 				result+=escapeXmlChars(jsonTxtObj.__text);
 			else
@@ -395,7 +379,7 @@ function X2JS(config) {
 		}
 		return result;
 	}
-	
+
 	function parseJSONTextObject ( jsonTxtObj ) {
 		var result ="";
 
@@ -409,12 +393,12 @@ function X2JS(config) {
 				else
 					result+=jsonTxtObj;
 			}
-		
+
 		return result;
 	}
-	
+
 	function parseJSONArray ( jsonArrRoot, jsonArrObj, attrList ) {
-		var result = ""; 
+		var result = "";
 		if(jsonArrRoot.length == 0) {
 			result+=startTag(jsonArrRoot, jsonArrObj, attrList, true);
 		}
@@ -422,35 +406,35 @@ function X2JS(config) {
 			for(var arIdx = 0; arIdx < jsonArrRoot.length; arIdx++) {
 				result+=startTag(jsonArrRoot[arIdx], jsonArrObj, parseJSONAttributes(jsonArrRoot[arIdx]), false);
 				result+=parseJSONObject(jsonArrRoot[arIdx]);
-				result+=endTag(jsonArrRoot[arIdx],jsonArrObj);						
+				result+=endTag(jsonArrRoot[arIdx],jsonArrObj);
 			}
 		}
 		return result;
 	}
-	
+
 	function parseJSONObject ( jsonObj ) {
-		var result = "";	
+		var result = "";
 
 		var elementsCnt = jsonXmlElemCount ( jsonObj );
-		
+
 		if(elementsCnt > 0) {
 			for( var it in jsonObj ) {
-				
+
 				if(jsonXmlSpecialElem ( jsonObj, it) )
-					continue;			
-				
-				var subObj = jsonObj[it];						
-				
-				var attrList = parseJSONAttributes( subObj )
-				
+					continue;
+
+				var subObj = jsonObj[it];
+
+				var attrList = parseJSONAttributes( subObj );
+
 				if(subObj == null || subObj == undefined) {
 					result+=startTag(subObj, it, attrList, true);
 				}
 				else
 				if(subObj instanceof Object) {
-					
-					if(subObj instanceof Array) {					
-						result+=parseJSONArray( subObj, it, attrList );					
+
+					if(subObj instanceof Array) {
+						result+=parseJSONArray( subObj, it, attrList );
 					}
 					else if(subObj instanceof Date) {
 						result+=startTag(subObj, it, attrList, false);
@@ -477,51 +461,10 @@ function X2JS(config) {
 			}
 		}
 		result+=parseJSONTextObject(jsonObj);
-		
+
 		return result;
 	}
-	
-	this.parseXmlString = function(xmlDocStr) {
-		var isIEParser = window.ActiveXObject || "ActiveXObject" in window;
-		if (xmlDocStr === undefined) {
-			return null;
-		}
-		var xmlDoc;
-		if (window.DOMParser) {
-			var parser=new window.DOMParser();			
-			var parsererrorNS = null;
-			// IE9+ now is here
-			if(!isIEParser) {
-				try {
-					parsererrorNS = parser.parseFromString("INVALID", "text/xml").childNodes[0].namespaceURI;
-				}
-				catch(err) {					
-					parsererrorNS = null;
-				}
-			}
-			try {
-				xmlDoc = parser.parseFromString( xmlDocStr, "text/xml" );
-				if( parsererrorNS!= null && xmlDoc.getElementsByTagNameNS(parsererrorNS, "parsererror").length > 0) {
-					//throw new Error('Error parsing XML: '+xmlDocStr);
-					xmlDoc = null;
-				}
-			}
-			catch(err) {
-				xmlDoc = null;
-			}
-		}
-		else {
-			// IE :(
-			if(xmlDocStr.indexOf("<?")==0) {
-				xmlDocStr = xmlDocStr.substr( xmlDocStr.indexOf("?>") + 2 );
-			}
-			xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-			xmlDoc.async="false";
-			xmlDoc.loadXML(xmlDocStr);
-		}
-		return xmlDoc;
-	};
-	
+
 	this.asArray = function(prop) {
 		if (prop === undefined || prop == null)
 			return [];
@@ -531,17 +474,17 @@ function X2JS(config) {
 		else
 			return [prop];
 	};
-	
+
 	this.toXmlDateTime = function(dt) {
 		if(dt instanceof Date)
 			return dt.toISOString();
 		else
 		if(typeof(dt) === 'number' )
 			return new Date(dt).toISOString();
-		else	
+		else
 			return null;
 	};
-	
+
 	this.asDateTime = function(prop) {
 		if(typeof(prop) == "string") {
 			return fromXmlDateTime(prop);
@@ -553,9 +496,9 @@ function X2JS(config) {
 	this.xml2json = function (xmlDoc) {
 		return parseDOMChildren ( xmlDoc );
 	};
-	
+
 	this.xml_str2json = function (xmlDocStr) {
-		var xmlDoc = this.parseXmlString(xmlDocStr);
+		var xmlDoc = Titanium.XML.parseString(xmlDocStr);
 		if(xmlDoc!=null)
 			return this.xml2json(xmlDoc);
 		else
@@ -567,12 +510,12 @@ function X2JS(config) {
 	};
 
 	this.json2xml = function (jsonObj) {
-		var xmlDocStr = this.json2xml_str (jsonObj);
-		return this.parseXmlString(xmlDocStr);
+		var xmlDocStr = this.json2xml_str(jsonObj);
+		return Titanium.XML.parseString(xmlDocStr);
 	};
-	
-	this.getVersion = function () {
-		return VERSION;
-	};
-	
+
+	//this.getVersion = function () {
+	//	return VERSION;
+	//};
+
 }
